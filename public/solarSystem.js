6714a5376447a0bbd7e00f6f076f2f3d7339a8db
Gabriel Grabalos <bielgraba@gmail.com>
2024-02-22 12:13:48 -0300

    Commented solar system initialization

--- public/solarSystem.js
+++ public/solarSystem.js
@@ -1,17 +1,8 @@
 class SolarSystem {
     constructor(data) {
-        this.mainCelestialBody = null;
         this.celestialBodies = [];
 
         this.initializeSolarSystem(data);
-    }
-
-    get MainCelestialBody() {
-        return this.mainCelestialBody;
-    }
-
-    set MainCelestialBody(value) {
-        this.mainCelestialBody = value;
     }
 
     get CelestialBodies() {
@@ -22,21 +13,6 @@ class SolarSystem {
         this.celestialBodies = value;
     }
 
-    /**
-     * 
-     * const data = [
-     *        { id: 1, type: 'star', radius: 250, color: "#ffcc00" },                                                        // Sun
-     *        { id: 2, type: 'planet', orbitalPeriod: 1, radius: 20,  color: "#e09f3e", distanceToParent: 1000, parent: 1 }, // Mercury
-     *        { id: 3, type: 'planet', orbitalPeriod: 1, radius: 18,  color: "#ca6702", distanceToParent: 1750, parent: 1 }, // Venus
-     *        { id: 4, type: 'planet', orbitalPeriod: 1, radius: 30,  color: "#0a9396", distanceToParent: 2500, parent: 1 }, // Earth
-     *        { id: 5, type: 'planet', orbitalPeriod: 1, radius: 15,  color: "#9b2226", distanceToParent: 3250, parent: 1 }, // Mars
-     *        { id: 6, type: 'planet', orbitalPeriod: 1, radius: 150, color: "#99582a", distanceToParent: 4000, parent: 1 }, // Jupiter
-     *        { id: 7, type: 'planet', orbitalPeriod: 1, radius: 150, color: "#fec89a", distanceToParent: 4750, parent: 1 }, // Saturn
-     *        { id: 8, type: 'planet', orbitalPeriod: 1, radius: 15,  color: "#118ab2", distanceToParent: 5500, parent: 1 }, // Uranus
-     *        { id: 9, type: 'planet', orbitalPeriod: 1, radius: 24,  color: "#073b4c", distanceToParent: 6250, parent: 1 }  // Neptune
-     *    ];
-     */
-
     initializeSolarSystem(data) {
         const celestialBodiesMap = {};
 
@@ -44,12 +20,18 @@ class SolarSystem {
             const { id, type, parent, ...props } = bodyData;
             let parentBody;
 
+            // If the celestial body has a parent,
+            // then get the parent celestial body:
             if (parent) {
+
+                // Get the parent celestial body from the map:
                 parentBody = celestialBodiesMap[parent];
+
                 if (!parentBody) {
                     throw new Error(`Parent with ID ${parent} not found for celestial body with ID ${id}`);
                 }
 
+                // Initialize orbit:
                 let orbit = {
                     parentCelestialBody: parentBody,
                     distance: props.distanceToParent,
@@ -60,17 +42,16 @@ class SolarSystem {
             }
 
 
-            const celestialBody = this.createCelestialBody(type, props, parentBody);
+            // Create the celestial body and add it to the celestial bodies array:
+            const celestialBody = this.createCelestialBody(type, props);
+            
             this.celestialBodies.push(celestialBody);
-            celestialBodiesMap[id] = celestialBody;
 
-            if (parentBody) {
-                celestialBody.orbit.parentCelestialBody = parentBody;
-            }
+            celestialBodiesMap[id] = celestialBody;
         });
     }
 
-    createCelestialBody(type, props, parentBody) {
+    createCelestialBody(type, props) {
         let celestialBody;
 
         switch (type) {
